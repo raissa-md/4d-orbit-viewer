3519971a93c395fa2c28b397ea583fa2c7e709b2
Raissa Woodland <raissa.woodland@nasa.gov>
2026-05-29 18:05:51 -0400

    Updated version number to 0-2026.05.29.1804

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const TITLE = "4D Orbit Viewer"
 export const CODE_SORCERESS = "Raissa Woodland"
-export const BUILD = "0-2026.05.18.1615"
-export const BUILD_DATE = "May 18, 2026"
+export const BUILD = "0-2026.05.29.1804"
+export const BUILD_DATE = "May 29, 2026"
 // Use npm run build to build
 
 // export const SSC_WS_ACCESS = 'https://sscweb-dev.sci.gsfc.nasa.gov/'
